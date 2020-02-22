779b19cc3d0d32de784fe1c4f83329be8f2499b0
Zachary Godfrey <zachary.godfrey@bamtech.net>
2020-02-21 21:44:33 -0500

    path resolution

--- client/build.js
+++ client/build.js
@@ -2,13 +2,14 @@ const path = require('path');
 const fs = require('fs-extra');
 const scss = require('node-sass');
 
-const readFile = (filePath) => fs.readFileSync(path.resolve(filePath), 'utf-8');
-const writeFile = (filePath, content) => fs.outputFileSync(path.resolve(filePath), content, 'utf-8');
+const absolutePath = (relativePath) => path.resolve(`${__dirname}/${relativePath}`)
+const readFile = (filePath) => fs.readFileSync(absolutePath(filePath), 'utf-8');
+const writeFile = (filePath, content) => fs.outputFileSync(absolutePath(filePath), content, 'utf-8');
 const compileSCSS = (content) => scss.renderSync({ data: content, outputStyle: 'compressed' }).css.toString().trim('\n');
 
 const index = readFile('./index.html');
 const style = readFile('./style.scss');
 const script = readFile('./script.js');
 
-fs.emptyDirSync(path.resolve('../dist'));
+fs.emptyDirSync(absolutePath('../dist'));
 writeFile('../dist/index.html', index.replace('/* style */', compileSCSS(style)).replace('/* script */', script));
