c0d10f37d0c9e060c6e6e1efac23ca8f20b4aeb5
Zachary Godfrey <ZacharyGodfrey@users.noreply.github.com>
2020-02-22 09:56:45 -0500

    Update build.js

--- client/build.js
+++ client/build.js
@@ -7,9 +7,9 @@ const readFile = (filePath) => fs.readFileSync(absolutePath(filePath), 'utf-8');
 const writeFile = (filePath, content) => fs.outputFileSync(absolutePath(filePath), content, 'utf-8');
 const compileSCSS = (content) => scss.renderSync({ data: content, outputStyle: 'compressed' }).css.toString().trim('\n');
 
-const index = readFile('./index.html');
 const style = compileSCSS(readFile('./style.scss'));
 const script = readFile('./script.js');
+const index = readFile('./index.html').replace('/* style */', style).replace('/* script */', script);
 
 fs.emptyDirSync(absolutePath('../dist'));
-writeFile('../dist/index.html', index.replace('/* style */', style).replace('/* script */', script));
+writeFile('../dist/index.html', index);
